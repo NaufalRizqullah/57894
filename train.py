afdffd45559fcb04ad59f1ec612881b1d0b99ce2
Muhammad Naufal Rizqullah <37763569+NaufalRizqullah@users.noreply.github.com>
2024-10-04 22:55:32 +0800

    feat: update args

--- train.py
+++ train.py
@@ -9,20 +9,21 @@ from data.dataloader import FaceToComicDataModule
 
 # Add argparser for config params
 parser = argparse.ArgumentParser()
-parser.add_argument("--load_checkpoint", action='store_true', help="Load checkpoint if this flag is set")
-parser.add_argument("--no_load_checkpoint", action='store_false', dest='load_checkpoint', help="Do not load checkpoint")
-parser.add_argument("--ckpt_path", type=str, default=config.CKPT_PATH, help="Path to checkpoint file")
-parser.add_argument("--learning_rate", type=float, default=config.LEARNING_RATE)
-parser.add_argument("--l1_lambda", type=int, default=config.L1_LAMBDA)
-parser.add_argument("--features_discriminator", type=int, nargs='+', default=config.FEATURE_DISCRIMINATOR)
-parser.add_argument("--features_generator", type=int, default=config.FEATURE_GENERATOR)
-parser.add_argument("--display_step", type=int, default=config.DISPLAY_STEP)
-parser.add_argument("--num_epoch", type=int, default=config.NUM_EPOCH)
-parser.add_argument("--path_face", type=str, default=config.PATH_FACE)
-parser.add_argument("--path_comic", type=str, default=config.PATH_COMIC)
-parser.add_argument("--image_size", type=int, default=config.IMAGE_SIZE)
-parser.add_argument("--batch_size", type=int, default=config.BATCH_SIZE)
-parser.add_argument("--max_samples", type=int, default=config.MAX_SAMPLES)
+parser.add_argument("--load_checkpoint", action='store_true', help="Load checkpoint if this flag is set. If not set, start training from scratch.")
+parser.add_argument("--no_load_checkpoint", action='store_false', dest='load_checkpoint', help="Do not load checkpoint. If set, start training from scratch.")
+
+parser.add_argument("--ckpt_path", type=str, default=config.CKPT_PATH, help="Path to checkpoint file. If load_checkpoint is set, this path will be used to load the checkpoint.")
+parser.add_argument("--learning_rate", type=float, default=config.LEARNING_RATE, help="Learning rate for Adam optimizer.")
+parser.add_argument("--l1_lambda", type=int, default=config.L1_LAMBDA, help="Scale factor for L1 loss.")
+parser.add_argument("--features_discriminator", type=int, nargs='+', default=config.FEATURE_DISCRIMINATOR, help="List of feature sizes for the discriminator network.")
+parser.add_argument("--features_generator", type=int, default=config.FEATURE_GENERATOR, help="Feature size for the generator network.")
+parser.add_argument("--display_step", type=int, default=config.DISPLAY_STEP, help="Interval of epochs to display loss and save examples.")
+parser.add_argument("--num_epoch", type=int, default=config.NUM_EPOCH, help="Number of epochs to train for.")
+parser.add_argument("--path_face", type=str, default=config.PATH_FACE, help="Path to folder containing face images.")
+parser.add_argument("--path_comic", type=str, default=config.PATH_COMIC, help="Path to folder containing comic images.")
+parser.add_argument("--image_size", type=int, default=config.IMAGE_SIZE, help="Size of input images.")
+parser.add_argument("--batch_size", type=int, default=config.BATCH_SIZE, help="Batch size for training.")
+parser.add_argument("--max_samples", type=int, default=config.MAX_SAMPLES, help="Maximum number of samples to use for training. If set to None, all samples will be used.")
 
 args = parser.parse_args()
 
@@ -62,7 +63,7 @@ trainer = L.Trainer(
     callbacks=[MyCustomSavingCallback()],
     default_root_dir="/kaggle/working/",
     precision="16-mixed",
-    fast_dev_run=True
+    # fast_dev_run=True
 )
 
 # Lightning DataModule
