516fbf944cc9df9554ffa9186f53d4adcb102e5f
Muhammad Naufal Rizqullah <37763569+NaufalRizqullah@users.noreply.github.com>
2024-10-19 07:38:08 +0800

    feat: add some changes in exampe and code

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ model = load_model_weights(
 
 # Transformation
 transform_face = T.Compose([
-        T.CenterCrop(config.IMAGE_SIZE),
-        T.Resize(config.IMAGE_SIZE),
+        T.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
         T.ToTensor(),
         T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 ])
@@ -29,12 +28,23 @@ transform_face = T.Compose([
 def inference(image: Image):
     # transforms the target image and add a batch dimension
     img = transform_face(image)
-    img = img.unsqueeze(0)
+    img_un = img.unsqueeze(0)
+
+    image_transform = img_un * 0.5 + 0.5 # Normalize from Tanh
+    im_detach = image_transform.detach().cpu().squeeze(0)
+    im_permute = im_detach.permute(1, 2, 0)
+    im_array = im_permute.numpy()
+    
+    # Scale values to 0-255 range
+    im_array = (im_array * 255).astype(np.uint8)
+    
+    # Convert numpy array to PIL Image
+    im_pil = Image.fromarray(im_array)
 
     # Inference the image
     model.eval()
     with torch.inference_mode():
-        c2f = model(img)
+        c2f = model(img_un)
     
     c2f = c2f * 0.5 + 0.5 # Normalize from Tanh
     image_unflat = c2f.detach().cpu().squeeze(0)  # Remove batch dimension
@@ -49,12 +59,15 @@ def inference(image: Image):
     # Convert numpy array to PIL Image
     image = Image.fromarray(image_array)
     
-    return image
+    return im_pil, image
 
 demo = gr.Interface(
     fn=inference,
     inputs=gr.Image(type="pil"),
-    outputs=gr.Image(),
+    outputs=[
+        gr.Image(label="Original after Transform"),
+        gr.Image(label="Converted by Model")
+    ],
     title="Pix2Pix Face to Comic",
     description="A implementation Pix2Pix from Scratch Pytorch",
     examples=[f"data/examples/{i}" for i in os.listdir("data/examples") if i.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
